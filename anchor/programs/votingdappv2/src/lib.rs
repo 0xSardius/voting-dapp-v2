a947aa0af074308ed1ade286dc28c59db46b38d4
0xSardius <0xSardius@gmail.com>
2024-12-23 21:36:35 -0500

    gm

--- anchor/programs/votingdappv2/src/lib.rs
+++ anchor/programs/votingdappv2/src/lib.rs
@@ -5,66 +5,10 @@ use anchor_lang::prelude::*;
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
 #[program]
-pub mod votingdappv2 {
+pub mod votingdapp {
     use super::*;
 
-  pub fn close(_ctx: Context<CloseVotingdappv2>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.votingdappv2.count = ctx.accounts.votingdappv2.count.checked_sub(1).unwrap();
-    Ok(())
-  }
-
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.votingdappv2.count = ctx.accounts.votingdappv2.count.checked_add(1).unwrap();
-    Ok(())
-  }
-
-  pub fn initialize(_ctx: Context<InitializeVotingdappv2>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.votingdappv2.count = value.clone();
-    Ok(())
-  }
-}
-
-#[derive(Accounts)]
-pub struct InitializeVotingdappv2<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  init,
-  space = 8 + Votingdappv2::INIT_SPACE,
-  payer = payer
-  )]
-  pub votingdappv2: Account<'info, Votingdappv2>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseVotingdappv2<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub votingdappv2: Account<'info, Votingdappv2>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub votingdappv2: Account<'info, Votingdappv2>,
-}
-
-#[account]
-#[derive(InitSpace)]
-pub struct Votingdappv2 {
-  count: u8,
+    pub fn initialize_poll(_ctx: Context<InitializePoll>, _poll_id: u64) -> Result<()> {
+        Ok(())
+    }
 }
