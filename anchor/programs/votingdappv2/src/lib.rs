ec69c538ad5a59d04b0635f5da70725cf7535923
0xSardius <0xSardius@gmail.com>
2024-12-26 16:59:43 -0500

    added InitializeCandiddate

--- anchor/programs/votingdappv2/src/lib.rs
+++ anchor/programs/votingdappv2/src/lib.rs
@@ -16,23 +16,59 @@ pub mod votingdappv2 {
         poll.poll_end = poll_end;
         Ok(())
     }
+
+    pub fn initialize_candidate(ctx: Context<InitializeCandidate>, candidate_name: String, poll_id: u64) -> Result<()> {
+
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
-#[instruction(poll_id: u64)]
+#[instruction(candidate_name: String, poll_id: u64)] // these have to be in the same order as the struct fields
+pub struct InitializeCandidate<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        seeds = [poll_id.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub poll: Account<'info, Poll>,
+
+    pub system_program: Program<'info, System>,
+}
+
+
+
+#[derive(Accounts)]
+#[instruction(candidate_name: String, poll_id: u64)]
 pub struct InitializePoll<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
     #[account(
-      init,
-      payer = signer,
-      space = 8 + Poll::INIT_SPACE,
+      
       seeds = [poll_id.to_le_bytes().as_ref()],
       bump,
     )]
     pub poll: Account<'info, Poll>,
 
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + Candidate::INIT_SPACE,
+        seeds = [poll_id.to_le_bytes().as_ref(), candidate_name.as_bytes()],
+        bump,
+    )]
+    pub candidate: Account<'info, Candidate>,
+
     pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Candidate {
+    #[max_len(280)]
+    pub candidate_name: String,
+    pub candidate_votes: u64,
 }
 
 #[account]
--- anchor/target/types/votingdappv2.ts
+++ anchor/target/types/votingdappv2.ts
@@ -14,6 +14,51 @@ export type Votingdappv2 = {
   },
   "instructions": [
     {
+      "name": "initializeCandidate",
+      "discriminator": [
+        210,
+        107,
+        118,
+        204,
+        255,
+        97,
+        112,
+        26
+      ],
+      "accounts": [
+        {
+          "name": "signer",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "poll",
+          "pda": {
+            "seeds": [
+              {
+                "kind": "arg",
+                "path": "pollId"
+              }
+            ]
+          }
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": [
+        {
+          "name": "candidateName",
+          "type": "string"
+        },
+        {
+          "name": "pollId",
+          "type": "u64"
+        }
+      ]
+    },
+    {
       "name": "initializePoll",
       "discriminator": [
         193,
@@ -33,12 +78,27 @@ export type Votingdappv2 = {
         },
         {
           "name": "poll",
+          "pda": {
+            "seeds": [
+              {
+                "kind": "arg",
+                "path": "pollId"
+              }
+            ]
+          }
+        },
+        {
+          "name": "candidate",
           "writable": true,
           "pda": {
             "seeds": [
               {
                 "kind": "arg",
                 "path": "pollId"
+              },
+              {
+                "kind": "arg",
+                "path": "candidateName"
               }
             ]
           }
@@ -70,6 +130,19 @@ export type Votingdappv2 = {
   ],
   "accounts": [
     {
+      "name": "candidate",
+      "discriminator": [
+        86,
+        69,
+        250,
+        96,
+        193,
+        10,
+        222,
+        123
+      ]
+    },
+    {
       "name": "poll",
       "discriminator": [
         110,
@@ -84,6 +157,22 @@ export type Votingdappv2 = {
     }
   ],
   "types": [
+    {
+      "name": "candidate",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "candidateName",
+            "type": "string"
+          },
+          {
+            "name": "candidateVotes",
+            "type": "u64"
+          }
+        ]
+      }
+    },
     {
       "name": "poll",
       "type": {
